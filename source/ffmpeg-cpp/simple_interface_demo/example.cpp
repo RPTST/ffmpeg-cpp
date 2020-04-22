29ec985a5bd025c7eb0cdb0557dde94dbbab0729
erba <eric.bachard@free.fr>
2020-04-22 23:30:15 +0200

    Add all the changes making the demo work

--- source/ffmpeg-cpp/simple_interface_demo/example.cpp
+++ source/ffmpeg-cpp/simple_interface_demo/example.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	void* handle = ffmpegCppCreate("out.mp4");
-	ffmpegCppAddVideoStream(handle, "samples/big_buck_bunny.mp4");
+	ffmpegCppAddVideoStream(handle, "../samples/big_buck_bunny.mp4");
 	ffmpegCppAddVideoFilter(handle, "transpose=cclock[middle];[middle]vignette");
-	ffmpegCppAddAudioStream(handle, "samples/big_buck_bunny.mp4");
+	ffmpegCppAddAudioStream(handle, "../samples/big_buck_bunny.mp4");
 	ffmpegCppGenerate(handle);
 	ffmpegCppClose(handle);
 }
