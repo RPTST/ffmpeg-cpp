86da654392c447b229a22654ad2d46b84bba25a3
erba <eric.bachard@free.fr>
2020-04-22 17:50:05 +0200

    Fix yet another signed/unsigned comparizon

--- source/ffmpeg-cpp/ffmpeg-cpp/Codecs/AudioCodec.cpp
+++ source/ffmpeg-cpp/ffmpeg-cpp/Codecs/AudioCodec.cpp
@@ -79,7 +79,7 @@ namespace ffmpegcpp
 
 	bool AudioCodec::IsChannelsSupported(int channels)
 	{
-		int64_t channelLayout = av_get_default_channel_layout(channels);
+		unsigned long int channelLayout = av_get_default_channel_layout(channels);
 		const uint64_t *p = codecContext->codec->channel_layouts;
 		while (*p)
 		{
