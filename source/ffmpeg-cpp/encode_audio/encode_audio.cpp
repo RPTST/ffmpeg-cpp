33409b14d776912cc36ee4a41c38dad3cf9492fb
erba <eric.bachard@free.fr>
2020-04-22 21:09:45 +0200

    Change for something different. Don't forget to installe libmp3lame btw git add encode_audio.cpp

--- source/ffmpeg-cpp/encode_audio/encode_audio.cpp
+++ source/ffmpeg-cpp/encode_audio/encode_audio.cpp
@@ -13,18 +13,22 @@ int main()
 	{
 		// Create a muxer that will output as MP3.
 		Muxer* muxer = new Muxer("output.mp3");
+#ifdef DEBUG
+            std::cout <<  "muxer created "<< "\n";
+#endif
 
 		// Create a MP3 codec that will encode the raw data.
-		AudioCodec* codec = new AudioCodec(AV_CODEC_ID_MP3);
+		AudioCodec codec(AV_CODEC_ID_MP3);
+                AudioCodec* p_codec = &codec;
 
 		// Create an encoder that will encode the raw audio data as MP3.
 		// Tie it to the muxer so it will be written to the file.
-		AudioEncoder* encoder = new AudioEncoder(codec, muxer);
+		AudioEncoder* encoder = new AudioEncoder(p_codec, muxer);
 
 		// Load the raw audio file so we can process it.
 		// We need to provide some info because we can't derive it from the raw format.
 		// Hand it the encoder so it will pass on its raw data to the encoder, which will in turn pass it on to the muxer.
-		const char* rawAudioFile = "samples/Vivaldi_s16le_2_channels_samplerate_11025.dat";
+		const char* rawAudioFile = "Vivaldi_s16le_2_channels_samplerate_11025.dat";
 		const char* rawAudioFormat = "s16le"; int rawAudioSampleRate = 11025; int rawAudioChannels = 2;
 		RawAudioFileSource* audioFile = new RawAudioFileSource(rawAudioFile, rawAudioFormat, rawAudioSampleRate, rawAudioChannels, encoder);
 
@@ -34,6 +38,7 @@ int main()
 		// Push all the remaining frames through.
 		while (!audioFile->IsDone())
 		{
+		    cerr << "pushing audio frame" << "\n";
 			audioFile->Step();
 		}
 		
@@ -42,13 +47,13 @@ int main()
 	}
 	catch (FFmpegException e)
 	{
-		cerr << "Exception caught!" << endl;
-		cerr << e.what() << endl;
+		cerr << "Exception caught!" << "\n";
+                std::cout << "Are you sure you installed libmp3lame ? " <<  "\n";
 		throw e;
 	}
 
 	cout << "Encoding complete!" << endl;
-	cout << "Press any key to continue..." << endl;
+	cout << "Press any key to continue..." << "\n";
 
 	getchar();
 }
