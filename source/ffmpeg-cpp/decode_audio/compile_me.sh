a8b5a9eb25187619e6301b3e64ba03f37822d973
erba <eric.bachard@free.fr>
2020-04-23 10:22:18 +0200

    Removing useless file

--- source/ffmpeg-cpp/decode_audio/compile_me.sh
+++
@@ -1,4 +0,0 @@
-g++ -I..  -Wall -std=c++11  \
-          -o read_audio decode_audio.cpp \
-          -L../../../build/ -lffmpeg-cpp \
-          `pkg-config --cflags --libs libavutil libavcodec libswscale libavformat`
