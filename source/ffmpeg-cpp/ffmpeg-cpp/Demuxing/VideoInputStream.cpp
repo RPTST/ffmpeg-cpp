19ed583f0c477444b9693ff37517f815b5b21c1b
erba <eric.bachard@free.fr>
2020-04-29 19:27:45 +0200

    Remove obsolete stuff

--- source/ffmpeg-cpp/ffmpeg-cpp/Demuxing/VideoInputStream.cpp
+++ source/ffmpeg-cpp/ffmpeg-cpp/Demuxing/VideoInputStream.cpp
@@ -45,15 +45,6 @@ namespace ffmpegcpp
 			throw new FFmpegException("Failed to read parameters from stream");
 		}
 
-		// DEPRECATED : all the parameters below seem to be implicitely copied
-		// https://ffmpeg.org/doxygen/3.4/libavcodec_2utils_8c_source.html#l02354
-		//codecContext->properties = stream->codec->properties;
-		//codecContext->codec = stream->codec->codec;
-		//codecContext->qmin = stream->codec->qmin;
-		//codecContext->qmax = stream->codec->qmax;
-		//codecContext->coded_width = stream->codec->coded_width;
-		//codecContext->coded_height = stream->codec->coded_height;
-
 		info.bitRate = CalculateBitRate(codecContext);
 
 		AVCodec* codec = CodecDeducer::DeduceDecoder(codecContext->codec_id);
