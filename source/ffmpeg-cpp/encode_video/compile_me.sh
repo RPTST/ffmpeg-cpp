a50f403bbea2a823299e87772b0882fa74e935e3
erba <eric.bachard@free.fr>
2020-04-30 09:42:14 +0200

    Waiting for a new CMakefile or simply a Makefile, rather separate every type of demo

--- source/ffmpeg-cpp/encode_video/compile_me.sh
+++ source/ffmpeg-cpp/encode_video/compile_me.sh
@@ -6,8 +6,35 @@
 # This document is under GPL v2 license
 # see : http://www.gnu.org/licenses/gpl-2.0.html
 #
-g++ -g -DDEBUG -I.. -Wall -std=c++11  \
+
+
+#DEBG="-g -DDEBUG"
+
+#BUIlD_DIR="./build"
+
+g++ -g ${DEBG} -I.. -Wall -std=c++11  \
           -I../ffmpeg-cpp \
-          -o encode_video encode_video.cpp \
+          -o encode_video_to_MPEG2 encode_to_MPEG2_video.cpp \
           -L../../../build/ -lffmpeg-cpp \
           `pkg-config --cflags --libs libavutil libavcodec libswscale libavformat libavdevice`
+
+
+g++ ${DEBG} -I.. -Wall -std=c++11  \
+          -I../ffmpeg-cpp \
+          -o encode_video_to_MPEG4 encode_to_MPEG4_video.cpp \
+          -L../../../build/ -lffmpeg-cpp \
+          `pkg-config --cflags --libs libavutil libavcodec libswscale libavformat libavdevice`
+
+
+g++ ${DEBG} -I.. -Wall -std=c++11  \
+          -I../ffmpeg-cpp \
+          -o encode_video_to_H264 encode_to_H264_video.cpp \
+          -L../../../build/ -lffmpeg-cpp \
+          `pkg-config --cflags --libs libavutil libavcodec libswscale libavformat libavdevice`
+
+g++ ${DEBG} -I.. -Wall -std=c++11  \
+          -I../ffmpeg-cpp \
+          -o encode_MJPEG_webcam_to_VP9_video encode_MJPEG_webcam_to_VP9_video.cpp \
+          -L../../../build/ -lffmpeg-cpp \
+          `pkg-config --cflags --libs libavutil libavcodec libswscale libavformat libavdevice`
+
